d3852a82885695d1c0b4f3ec1171ab5ae513ea3d
gskhaled <gaser.abdelrahman@student.guc.edu.eg>
2021-01-09 20:06:35 +0200

    added weapon switching, reloading and animations

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,30 +1,80 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     public float damage = 0f;
     public float range = 100f;
     public float fireRate = 1f;
+    public int maxMagazine = 10;
+    public float allAmmu = Mathf.Infinity;
+    public float reloadTime = 2f;
     public Camera FPSCam;
     public ParticleSystem muzzleFlash;
+    public Animator animator;
     public GameObject impactEffect;
     public float impactDuration = 0.2f;
     public float impactForce = 1f;
 
     private float nextTimeToFire = 0f;
+    private int currentAmmo = 0;
+    private bool isReloading = false;
+    void Start()
+    {
+        currentAmmo = maxMagazine;
+    }
 
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        if (isReloading)
+            return;
+
+        if(allAmmu > 0)
         {
-            nextTimeToFire = Time.time + (1f / fireRate);
-            Shoot();
+            if (currentAmmo <= 0 || (Input.GetKeyDown(KeyCode.R) && currentAmmo != maxMagazine))
+            {
+                StartCoroutine(Reload());
+                return;
+            }
+            if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + (1f / fireRate);
+                Shoot();
+            }
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        if (allAmmu > 0)
+        {
+            isReloading = true;
+            //Debug.Log("Reloading.....");
+            animator.SetBool("Reloading", true);
+            yield return new WaitForSeconds(reloadTime - .25f);
+            animator.SetBool("Reloading", false);
+            yield return new WaitForSeconds(.25f);
+            int ammu = (int) allAmmu - maxMagazine;
+            if (ammu >= 0)
+            {
+                currentAmmo = maxMagazine;
+            }
+            else
+                currentAmmo = (int) allAmmu;
+            allAmmu = ammu;
+            isReloading = false;
         }
     }
 
     void Shoot()
     {
         muzzleFlash.Play();
+        currentAmmo--;
         RaycastHit hit;
         if (Physics.Raycast(FPSCam.transform.position, FPSCam.transform.forward, out hit, range))
         {
