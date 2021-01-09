312eb76f845e3ae04bc6f449060f212d5db6e67a
gskhaled <gaser.abdelrahman@student.guc.edu.eg>
2021-01-09 18:20:29 +0200

    added the gun models, effects and logic

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 0f;
+    public float range = 100f;
+    public float fireRate = 1f;
+    public Camera FPSCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float impactDuration = 0.2f;
+    public float impactForce = 1f;
+
+    private float nextTimeToFire = 0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + (1f / fireRate);
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(FPSCam.transform.position, FPSCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
+        GameObject impactObj = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactObj, impactDuration);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
