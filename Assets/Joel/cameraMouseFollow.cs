acc2da70c7aaf3356ccc8d3815db8a031c2c6cea
MarwanElsadat <itsmarwan@gmail.com>
2021-01-10 02:08:03 +0200

    Joel model and animations

---
+++ Assets/Joel/cameraMouseFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMouseFollow : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start the mouse at the center
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        //playerBody.Rotate(Vector3.right * mouseY);
+
+        
+    }
+}
---
+++ Assets/Joel/playerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    int health = 300;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 300;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void incurDamage (int damage)
+    {
+        health -= damage;
+    }
+}
